561454e87a418c20a5f544fd4c54373ef1394694
22520565 <22520565@gm.uit.edu.vn>
2025-05-07 15:50:24 +0700

    Add allow origin localhost port 5174

--- backend/src/main/java/com/movie/main/config/WebConfig.java
+++ backend/src/main/java/com/movie/main/config/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(@NonNull final CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5174")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
