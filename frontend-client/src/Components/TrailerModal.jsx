3f0d352f0e3a90e6f24a5cd49933883704e9e9c3
irisus1 <146847404+irisus1@users.noreply.github.com>
2025-05-20 14:07:15 +0700

    Update TrailerModal.jsx

--- frontend-client/src/Components/TrailerModal.jsx
+++ frontend-client/src/Components/TrailerModal.jsx
@@ -5,6 +5,11 @@ import { IoClose } from "react-icons/io5"; // Import biểu tượng IoClose t�
 const TrailerModal = ({ videoOpen, setVideoOpen, videoUrl }) => {
   const modalRef = useRef(null);
 
+  const getEmbedUrl = (url) => {
+    const match = url.match(/v=([^&]+)/);
+    return match ? `https://www.youtube.com/embed/${match[1]}` : url;
+  };
+
   useEffect(() => {
     const handleOutsideClick = (event) => {
       if (modalRef.current && !modalRef.current.contains(event.target)) {
@@ -21,14 +26,14 @@ const TrailerModal = ({ videoOpen, setVideoOpen, videoUrl }) => {
   if (!videoOpen) return null;
 
   // Thêm autoplay và mute vào URL của video
-  const autoplayUrl = `${videoUrl}&autoplay=1`;
+  const embedUrl = `${getEmbedUrl(videoUrl)}?autoplay=1&mute=1`;
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-90">
       <div ref={modalRef} className="relative w-full max-w-[937px] bg-white">
         <iframe
           className="h-[528px] w-full"
-          src={autoplayUrl} // Sử dụng URL mới với autoplay và mute
+          src={embedUrl} // Sử dụng URL mới với autoplay và mute
           title="Trailer"
           allowFullScreen
           allow="autoplay" // Cho phép autoplay
