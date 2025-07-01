334fe0b485e75b88293b6d79daa2b2bd74bf1322
22520565 <22520565@gm.uit.edu.vn>
2025-07-01 15:41:27 +0700

    Fix `OrderItem`.

--- backend/src/main/java/com/movie/main/dto/response/OrderDecoratorsOfflineServiceResponseDto.java
+++
@@ -1,9 +0,0 @@
-package com.movie.main.dto.response;
-
-public record OrderDecoratorsOfflineServiceResponseDto(
-        int customerOrderId,
-        boolean printed,
-        boolean served,
-        String invalidReasonPrinted,
-        String invalidReasonServed) {
-}
--- backend/src/main/java/com/movie/main/dto/response/OrderDecoratorsPointUsageResponseDto.java
+++
@@ -1,7 +0,0 @@
-package com.movie.main.dto.response;
-
-public record OrderDecoratorsPointUsageResponseDto(
-        int customerOrderId,
-        int pointUsed,
-        int pointToMoneyRatio) {
-}
