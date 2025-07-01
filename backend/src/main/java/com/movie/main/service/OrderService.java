f4a3356ae5a3673e9345079dfb10184972485b2c
22520565 <22520565@gm.uit.edu.vn>
2025-07-01 23:34:38 +0700

    Fix `handleStripeWebhook` on `OrderService`.

--- backend/src/main/java/com/movie/main/service/OrderService.java
+++ backend/src/main/java/com/movie/main/service/OrderService.java
@@ -673,6 +673,7 @@ public class OrderService {
         }
     }
 
+    @Transactional
     public Expected<OrderResponseDto, HandleWebhookError> handleStripeWebhook(String sigHeader, String payload) {
         try {
             final var event = Webhook.constructEvent(payload, sigHeader, ResourceStrings.STRIPE_WEBHOOK_SECRET);
